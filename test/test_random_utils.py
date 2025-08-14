f63a01cd13381b4338caefb0cd62d5d6b85be83f
kyle <kyle@101010.org>
2025-08-14 12:12:20 -0500

    remove one of the test cases

--- test/test_random_utils.py
+++ test/test_random_utils.py
@@ -3,7 +3,3 @@ from src.random_utils import RandomUtils
 def test_is_palindrome():
     random_utils = RandomUtils()
     assert random_utils.is_palindrome("racecar")
-
-def test_is_valid_email():
-    random_utils = RandomUtils()
-    assert random_utils.is_valid_email("racecar") is False
